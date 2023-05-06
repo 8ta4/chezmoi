a5200452ed78bcd245047f092c6d36e9eb0e3431
a <a@as-Virtual-Machine.local>
2023-05-06 01:13:42 -0700

    Add new dot_tmux.conf.local file

--- install.sh
+++ install.sh
@@ -38,6 +38,11 @@ fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+## .tmux
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+
 ## chezmoi
 chezmoi apply --force
 
