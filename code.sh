9c27cc58243b2b0065ca7f39ae8242bf2732be68
a <a@as-Virtual-Machine.local>
2023-05-31 03:33:41 -0700

    Add GitHub Actions extension to code.sh

--- code.sh
+++ code.sh
@@ -1,3 +1,4 @@
 code --force --install-extension asvetliakov.vscode-neovim
 code --force --install-extension esbenp.prettier-vscode
 code --force --install-extension redhat.vscode-yaml
+code --force --install-extension GitHub.vscode-github-actions
