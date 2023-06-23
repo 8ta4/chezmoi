9fc3eb02e777f34edafbe17f8970e717c65f7c1f
a <a@as-Virtual-Machine.local>
2023-06-23 13:54:36 -0700

    Disable Spotlight hotkey in plist.sh

--- plist.sh
+++ plist.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 /usr/libexec/PlistBuddy -c "Add ':AppleInterfaceStyle' string Dark" "$HOME/Library/Preferences/.GlobalPreferences.plist"
+
+# Disable Spotlight hotkey
+/usr/libexec/PlistBuddy -c "Set ':AppleSymbolicHotKeys:64:enabled' false" "$HOME/Library/Preferences/com.apple.symbolichotkeys.plist"
