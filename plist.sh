feb9f7712ab50a439197a0cbb61d9e9a193eec6f
a <a@as-Virtual-Machine.local>
2023-06-24 12:52:09 -0700

    Enable clipboard access in iTerm2

--- plist.sh
+++ plist.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+## macOS
 /usr/libexec/PlistBuddy -c "Add ':AppleInterfaceStyle' string 'Dark'" "$HOME/Library/Preferences/.GlobalPreferences.plist"
 
 # Disable Spotlight hotkey
 /usr/libexec/PlistBuddy -c "Set ':AppleSymbolicHotKeys:64:enabled' 'false'" "$HOME/Library/Preferences/com.apple.symbolichotkeys.plist"
+
+## iTerm2
+# Enable clipboard access in "Prefs > General > Selection > Applications in terminal may access clipboard".
+/usr/libexec/PlistBuddy -c "Add ':AllowClipboardAccess' bool 'true'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
