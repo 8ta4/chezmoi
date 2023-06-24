e3f9316a68c20429f5eb0f6920e6a39f5c2c66ac
a <a@as-Virtual-Machine.local>
2023-06-24 13:10:20 -0700

    Add command + arrow shortcuts to iTerm2

--- plist.sh
+++ plist.sh
@@ -17,3 +17,11 @@
 # option + right arrow to jump word right
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x280000-0x0:Action' integer '10'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x280000-0x0:Text' string 'f'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+# command + left arrow to jump to beginning of line
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf702-0x300000-0x0:Action' integer '11'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf702-0x300000-0x0:Text' string '0x1'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+# command + right arrow to jump to end of line
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x300000-0x0:Action' integer '11'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x300000-0x0:Text' string '0x5'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
