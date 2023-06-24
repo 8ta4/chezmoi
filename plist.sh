fe1d583622b6f9e4d74dd82994471553e15c8285
a <a@as-Virtual-Machine.local>
2023-06-24 12:59:47 -0700

    Add option + arrow shortcuts to iTerm2

--- plist.sh
+++ plist.sh
@@ -9,3 +9,11 @@
 ## iTerm2
 # Enable clipboard access in "Prefs > General > Selection > Applications in terminal may access clipboard".
 /usr/libexec/PlistBuddy -c "Add ':AllowClipboardAccess' bool 'true'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+# option + left arrow to jump word left
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf702-0x280000-0x0:Action' integer '10'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf702-0x280000-0x0:Text' string 'b'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+# option + right arrow to jump word right
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x280000-0x0:Action' integer '10'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+/usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x280000-0x0:Text' string 'f'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
