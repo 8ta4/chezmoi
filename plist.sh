921eedfd257f81fdc694d9e60159af3df512b8e4
a <a@as-Virtual-Machine.local>
2023-06-25 12:38:43 -0700

    Add references to plist.sh

--- plist.sh
+++ plist.sh
@@ -11,6 +11,7 @@
 /usr/libexec/PlistBuddy -c "Add ':AllowClipboardAccess' bool 'true'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 
 # option + left arrow to jump word left
+# https://apple.stackexchange.com/a/218639
 /usr/libexec/PlistBuddy -c "Delete ':New Bookmarks:0:Keyboard Map:0xf702-0x280000'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf702-0x280000-0x0:Action' integer '10'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf702-0x280000-0x0:Text' string 'b'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
@@ -29,10 +30,12 @@
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0xf703-0x300000-0x0:Text' string '0x5'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 
 # option + delete to delete word left
+# https://stackoverflow.com/a/63403514
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0x7f-0x80000-0x0:Action' integer '11'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0x7f-0x80000-0x0:Text' string '0x1b 0x7f'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 
 # command + delete to delete to beginning of line
+# https://stackoverflow.com/a/53626025
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0x7f-0x100000-0x0:Action' integer '11'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 /usr/libexec/PlistBuddy -c "Add ':New Bookmarks:0:Keyboard Map:0x7f-0x100000-0x0:Text' string '0x15'" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 
