9cebce2f06284d61228d72929c7e60a95dcb20bd
a <a@as-Virtual-Machine.local>
2023-07-28 16:24:07 -0700

    Change the order of the extension list

--- extension.sh
+++ extension.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# uBlock Origin
-extension install chrome cjpalhdlnbpafiamejdnhcphjbkeiagm
-
-# Vimium
-extension install chrome dbepggeogbaibhgnhhndojpepiihcmeb
-
-# I still don't care about cookies
-extension install chrome edibdbjcniadpccecjdfdjjppcpchdlm
+# Buster: Captcha Solver for Humans
+extension install chrome mpbjkejclgfgadiemmefgebjfooflfhl
 
 # Dark Reader
 extension install chrome eimadpbcbfnmbkopoojfekhnkhdbieeh
 
-# Return YouTube Dislike
-extension install chrome gebbhagfogifgggkldgodflihgfeippi
-
-# SponsorBlock for YouTube - Skip Sponsorships
-extension install chrome mnjggcdmjocbbbhaepdhchncahnbgone
-
-# Buster: Captcha Solver for Humans
-extension install chrome mpbjkejclgfgadiemmefgebjfooflfhl
-
-# SingleFile
-extension install chrome mpiodijhokgodhhofbcjdecpffjipkle
-
-# Video Speed Controller
-extension install chrome nffaoalbilbmmfgbnbgppjihopabppdk
-
-# Password Alert
-extension install chrome noondiphcddnnabmjcihcjfbhfklnnep
+# I still don't care about cookies
+extension install chrome edibdbjcniadpccecjdfdjjppcpchdlm
 
 # KeePassXC-Browser
 extension install chrome oboonakemofpalcgghocfoadofidjkkk
 
 # Link to Text Fragment
 extension install chrome pbcodcjpfjdpcineamnnmbkkmkdpajjg
+
+# Password Alert
+extension install chrome noondiphcddnnabmjcihcjfbhfklnnep
+
+# Return YouTube Dislike
+extension install chrome gebbhagfogifgggkldgodflihgfeippi
+
+# SingleFile
+extension install chrome mpiodijhokgodhhofbcjdecpffjipkle
+
+# SponsorBlock for YouTube - Skip Sponsorships
+extension install chrome mnjggcdmjocbbbhaepdhchncahnbgone
+
+# uBlock Origin
+extension install chrome cjpalhdlnbpafiamejdnhcphjbkeiagm
+
+# Video Speed Controller
+extension install chrome nffaoalbilbmmfgbnbgppjihopabppdk
+
+# Vimium
+extension install chrome dbepggeogbaibhgnhhndojpepiihcmeb
