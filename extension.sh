4ed38998309669121a31785ee8295aa74bfd050e
a <a@as-Virtual-Machine.local>
2023-07-28 16:21:31 -0700

    Annotate extensions in extension.sh

--- extension.sh
+++ extension.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+# uBlock Origin
 extension install chrome cjpalhdlnbpafiamejdnhcphjbkeiagm
+
+# Vimium
 extension install chrome dbepggeogbaibhgnhhndojpepiihcmeb
+
+# I still don't care about cookies
 extension install chrome edibdbjcniadpccecjdfdjjppcpchdlm
+
+# Dark Reader
 extension install chrome eimadpbcbfnmbkopoojfekhnkhdbieeh
+
+# Return YouTube Dislike
 extension install chrome gebbhagfogifgggkldgodflihgfeippi
+
+# SponsorBlock for YouTube - Skip Sponsorships
 extension install chrome mnjggcdmjocbbbhaepdhchncahnbgone
+
+# Buster: Captcha Solver for Humans
 extension install chrome mpbjkejclgfgadiemmefgebjfooflfhl
+
+# SingleFile
 extension install chrome mpiodijhokgodhhofbcjdecpffjipkle
+
+# Video Speed Controller
 extension install chrome nffaoalbilbmmfgbnbgppjihopabppdk
+
+# Password Alert
 extension install chrome noondiphcddnnabmjcihcjfbhfklnnep
+
+# KeePassXC-Browser
 extension install chrome oboonakemofpalcgghocfoadofidjkkk
+
+# Link to Text Fragment
 extension install chrome pbcodcjpfjdpcineamnnmbkkmkdpajjg
