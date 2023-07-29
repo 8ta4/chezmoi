6dba6e570721ff273c3006c3f759838f9f425872
a <a@as-Virtual-Machine.local>
2023-07-28 19:46:57 -0700

    Add comments explaining changes to keybindings

--- extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
+++ extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
@@ -1,4 +1,5 @@
 chrome.storage.sync.set({
+  // Certain keys are disabled to prevent conflicting with Video Speed Controller's keybindings
   exclusionRules:
     '[{"pattern":"https?://mail.google.com/*","passKeys":""},{"pattern":"*","passKeys":"arsxz"}]',
 });
