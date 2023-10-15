99082ffd3e50b6f9e2696254b249c26f1f764f4a
a <a@as-Virtual-Machine.local>
2023-10-15 16:50:50 -0700

    Simplify output format by removing unnecessary instructions

--- commit.sh
+++ commit.sh
@@ -5,7 +5,7 @@ diff=$(git diff --cached)
 
 
 # Concatenate the output with the string
-output="Given the following code diff, write five clear, concise, and informative commit messages that accurately describe the change.\n\nEach message should begin with a capital letter and a verb in the imperative mood.\n\nOutput format:\n\`\`\`\nCommit message 1\nCommit message 2\nCommit message 3\nCommit message 4\nCommit message 5\n\`\`\`\n\nCode diff:\n\`\`\`\n$diff\n\`\`\`"
+output="Given the following code diff, write five clear, concise, and informative commit messages that accurately describe the change.\n\nEach message should begin with a capital letter and a verb in the imperative mood.\n\n\`\`\`\n$diff\n\`\`\`"
 
 # Output results as a list, not more than 6 items
 echo -e "$output" | pbcopy
