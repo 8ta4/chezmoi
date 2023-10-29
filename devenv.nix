2fe58bfc4a84198dfc3fcabccccf4f9d2eae401c
a <a@as-Virtual-Machine.local>
2023-10-29 01:46:39 -0700

    Add rubocop hook to pre-commit configuration

--- devenv.nix
+++ devenv.nix
@@ -26,6 +26,12 @@
   pre-commit.hooks = {
     nixpkgs-fmt.enable = true;
     prettier.enable = true;
+    rubocop = {
+      enable = true;
+      # https://github.com/rubocop/rubocop/blob/d67dd0334edeec9ca9d9ac1d625b8da2d46e622a/.pre-commit-hooks.yaml
+      entry = "${pkgs.rubyPackages.rubocop}/bin/rubocop -a";
+      types = [ "ruby" ];
+    };
     shellcheck = {
       enable = true;
       # https://www.shellcheck.net/wiki/SC1071
