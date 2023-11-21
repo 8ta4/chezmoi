fb752e08bfc25332691c96c4d4ab1ca4c408f766
a <a@as-Virtual-Machine.local>
2023-11-20 17:26:40 -0800

    Change the passKeys for the exclusion rules from arsxz to as

--- extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
+++ extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
@@ -1,5 +1,5 @@
 chrome.storage.sync.set({
   // Certain keys are disabled to prevent conflicting with Video Speed Controller's keybindings
   exclusionRules:
-    '[{"pattern":"https?://mail.google.com/*","passKeys":""},{"pattern":"*","passKeys":"arsxz"}]',
+    '[{"pattern":"https?://mail.google.com/*","passKeys":""},{"pattern":"*","passKeys":"as"}]',
 });
