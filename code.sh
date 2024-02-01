7ac74ff0244315eab5732cd26bacd95565d39ef8
a <a@as-Virtual-Machine.local>
2024-01-31 22:08:08 -0800

    Add GitLens extension to code.sh

--- code.sh
+++ code.sh
@@ -4,6 +4,7 @@ code --force --install-extension GitHub.vscode-github-actions
 code --force --install-extension asvetliakov.vscode-neovim
 code --force --install-extension betterthantomorrow.calva
 code --force --install-extension castwide.solargraph
+code --force --install-extension eamodio.gitlens
 code --force --install-extension esbenp.prettier-vscode
 code --force --install-extension haskell.haskell
 code --force --install-extension nwolverson.ide-purescript
