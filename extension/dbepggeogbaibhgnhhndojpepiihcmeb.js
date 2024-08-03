2d9195013f939e3db1ee40ef43feaf9c055879fd
a <a@as-Virtual-Machine.local>
2024-08-03 02:44:46 -0700

    Update exclusionRules to include 't' key in passKeys

--- extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
+++ extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
@@ -1,5 +1,5 @@
 chrome.storage.sync.set({
   // Certain keys are disabled to prevent conflicting with Video Speed Controller's keybindings
   exclusionRules:
-    '[{"pattern":"https?://mail.google.com/*","passKeys":""},{"pattern":"*","passKeys":"as"}]',
+    '[{"pattern":"https?://mail.google.com/*","passKeys":""},{"pattern":"*","passKeys":"ast"}]',
 });
