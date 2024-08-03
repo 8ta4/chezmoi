d5e859afe1e89a514d2e6cdabc6bf67ee85dc316
a <a@as-Virtual-Machine.local>
2024-08-03 03:08:39 -0700

    Remove unnecessary blank line in commit.sh

--- commit.sh
+++ commit.sh
@@ -3,7 +3,6 @@
 # Get git diff --cached output
 diff=$(git diff --cached)
 
-
 # Concatenate the output with the string
 output="Given the following code diff, write five clear, concise, and informative commit messages that accurately describe the change.\n\nEach message should begin with a capital letter and a verb in the imperative mood.\n\n\`\`\`\n$diff\n\`\`\`"
 
