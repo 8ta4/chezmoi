bde2f5145b7fb926da18bb6ebc3f0609dbb0b6c9
a <a@as-Virtual-Machine.local>
2024-11-16 15:45:18 -0800

    Add setting to hide end screen

--- extension/bnomihfieiccainjcjblhegjgglakjdd.js
+++ extension/bnomihfieiccainjcjblhegjgglakjdd.js
@@ -1,2 +1,3 @@
 chrome.storage.local.set({ header_position: "hover_on_video_page" });
 chrome.storage.local.set({ forced_theater_mode: true });
+chrome.storage.local.set({ player_hide_endscreen: true });
