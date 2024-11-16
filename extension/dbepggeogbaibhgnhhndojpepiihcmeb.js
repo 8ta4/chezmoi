9f501312244feaeb8b8277e35bc4c728be7b0e70
a <a@as-Virtual-Machine.local>
2024-11-16 02:01:49 -0800

    Update exclusion rules to include all keys

--- extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
+++ extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
@@ -1,7 +1,10 @@
 chrome.storage.sync.set({
   exclusionRules: [
     { passKeys: "", pattern: "https?://mail.google.com/*" },
-    // 't' conflicts with YouTube's theater mode
-    { passKeys: "t", pattern: "*" },
+    {
+      passKeys:
+        "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?",
+      pattern: "*",
+    },
   ],
 });
