8d0de4cd5915b74f1afbe38f8f9cfb2b68cb5c35
a <a@as-Virtual-Machine.local>
2024-11-16 15:56:00 -0800

    Add setting to hide related videos

--- extension/bnomihfieiccainjcjblhegjgglakjdd.js
+++ extension/bnomihfieiccainjcjblhegjgglakjdd.js
@@ -1,3 +1,4 @@
 chrome.storage.local.set({ header_position: "hover_on_video_page" });
 chrome.storage.local.set({ forced_theater_mode: true });
 chrome.storage.local.set({ player_hide_endscreen: true });
+chrome.storage.local.set({ related_videos: "hidden" });
