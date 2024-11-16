defbbe9edaae3e54e56450aab240619debcb51f1
a <a@as-Virtual-Machine.local>
2024-11-16 02:42:59 -0800

    Add workaround for Vimium issue #3190 by passing through most keyboard shortcuts

--- extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
+++ extension/dbepggeogbaibhgnhhndojpepiihcmeb.js
@@ -1,9 +1,15 @@
 chrome.storage.sync.set({
   exclusionRules: [
     { passKeys: "", pattern: "https?://mail.google.com/*" },
+    // https://github.com/philc/vimium/issues/3190
+    // Vimium doesn't support direct whitelisting of sites where it should be disabled.
+    // This is a workaround that passes through almost all keyboard shortcuts to the website
+    // by listing most common keys as "passKeys". This effectively disables Vimium's
+    // keyboard shortcuts globally, except for a few specific keys not listed here.
+    // The key order follows the physical MacBook Pro keyboard layout from left to right, top to bottom.
     {
       passKeys:
-        "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?",
+        "1234567890-=qwertyiop\\ashl;'zxcvbnm,./~!@#$%^&*()_+QERTYUIOP{}|ASDFHJKL:\"ZXCVBNM?",
       pattern: "*",
     },
   ],
