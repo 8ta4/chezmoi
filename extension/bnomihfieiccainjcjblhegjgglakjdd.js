d13fbe104dff3700926336b97b166bf72f2f0a44
a <a@as-Virtual-Machine.local>
2024-11-16 16:27:27 -0800

    Modify script to reorder player_hide_endscreen setting

--- extension/bnomihfieiccainjcjblhegjgglakjdd.js
+++ extension/bnomihfieiccainjcjblhegjgglakjdd.js
@@ -1,15 +1,3 @@
-chrome.storage.local.set({ header_position: "hover_on_video_page" });
-chrome.storage.local.set({ forced_theater_mode: true });
-chrome.storage.local.set({ player_hide_endscreen: true });
-chrome.storage.local.set({ related_videos: "hidden" });
-chrome.storage.local.set({ player_play_button: true });
-chrome.storage.local.set({ player_previous_button: true });
-chrome.storage.local.set({ player_next_button: true });
-chrome.storage.local.set({ player_subtitles_button: true });
-chrome.storage.local.set({ player_miniplayer_button: true });
-chrome.storage.local.set({ player_view_button: true });
-chrome.storage.local.set({ player_screen_button: true });
-chrome.storage.local.set({ subscribe: "hidden" });
 chrome.storage.local.set({
   shortcut_chapters: {
     alt: false,
@@ -28,3 +16,15 @@ chrome.storage.local.set({
     wheel: 0,
   },
 });
+chrome.storage.local.set({ header_position: "hover_on_video_page" });
+chrome.storage.local.set({ forced_theater_mode: true });
+chrome.storage.local.set({ related_videos: "hidden" });
+chrome.storage.local.set({ player_hide_endscreen: true });
+chrome.storage.local.set({ player_play_button: true });
+chrome.storage.local.set({ player_previous_button: true });
+chrome.storage.local.set({ player_next_button: true });
+chrome.storage.local.set({ player_subtitles_button: true });
+chrome.storage.local.set({ player_miniplayer_button: true });
+chrome.storage.local.set({ player_view_button: true });
+chrome.storage.local.set({ player_screen_button: true });
+chrome.storage.local.set({ subscribe: "hidden" });
