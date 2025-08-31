d683d90557c571bc328fb585929a0686d82339f5
a <a@as-Virtual-Machine.local>
2025-08-31 15:42:00 -0700

    Introduce telescope.nvim plugin via new Lua config file

---
+++ home/dot_config/nvim/lua/plugins/telescope.nvim.lua
@@ -0,0 +1,7 @@
+-- https://github.com/nvim-telescope/telescope.nvim/blob/b4da76be54691e854d3e0e02c36b0245f945c2c7/README.md?plain=1#L121-L125
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	-- or                              , branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
