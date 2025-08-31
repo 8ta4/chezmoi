a01f128d48d6b58ba6b0b5457097dca26d7670b6
a <a@as-Virtual-Machine.local>
2025-08-31 13:49:10 -0700

    Reference link to mini.nvim README for plugin context

--- home/dot_config/nvim/lua/plugins/mini.nvim.lua
+++ home/dot_config/nvim/lua/plugins/mini.nvim.lua
@@ -1,7 +1,10 @@
+-- https://github.com/nvim-mini/mini.nvim/blob/ddb70da6ec6aa896cfde87350d1e8dffb57ddef0/README.md?plain=1#L54
 return {
 	"nvim-mini/mini.nvim",
 	config = function()
+		-- https://github.com/nvim-mini/mini.nvim/blob/ddb70da6ec6aa896cfde87350d1e8dffb57ddef0/readmes/mini-ai.md?plain=1#L145
 		require("mini.ai").setup()
+		-- https://github.com/nvim-mini/mini.nvim/blob/ddb70da6ec6aa896cfde87350d1e8dffb57ddef0/readmes/mini-align.md?plain=1#L142
 		require("mini.align").setup()
 	end,
 	version = "*",
