fcce66d02ce781485843336ac4eb5b6dcede7fa6
a <a@as-Virtual-Machine.local>
2025-08-31 15:14:26 -0700

    Enable bracketed motions via mini.bracketed in mini.nvim

--- home/dot_config/nvim/lua/plugins/mini.nvim.lua
+++ home/dot_config/nvim/lua/plugins/mini.nvim.lua
@@ -6,6 +6,8 @@ return {
 		require("mini.ai").setup()
 		-- https://github.com/nvim-mini/mini.nvim/blob/ddb70da6ec6aa896cfde87350d1e8dffb57ddef0/readmes/mini-align.md?plain=1#L142
 		require("mini.align").setup()
+		-- https://github.com/nvim-mini/mini.nvim/blob/ddb70da6ec6aa896cfde87350d1e8dffb57ddef0/readmes/mini-bracketed.md?plain=1#L160
+		require("mini.bracketed").setup()
 		-- https://github.com/nvim-mini/mini.nvim/blob/ddb70da6ec6aa896cfde87350d1e8dffb57ddef0/readmes/mini-operators.md?plain=1#L136
 		require("mini.operators").setup()
 	end,
