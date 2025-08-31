cd16264128e3a4c4a63471835209d1e0af52ac5b
a <a@as-Virtual-Machine.local>
2025-08-30 22:10:53 -0700

    Add reference link to nvim-surround README for configuration context

--- home/dot_config/nvim/lua/plugins/nvim-surround.lua
+++ home/dot_config/nvim/lua/plugins/nvim-surround.lua
@@ -1,3 +1,4 @@
+-- https://github.com/kylechui/nvim-surround/blob/a868c256c861044beb9794b4dd126480dcdfbdad/README.md?plain=1#L40-L49
 return {
 	"kylechui/nvim-surround",
 	version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
