da5967b710266c93046346c076df80607b348777
a <a@as-Virtual-Machine.local>
2025-08-31 15:55:07 -0700

    Disable Telescope when running inside VSCode by gating with vim.g.vscode

--- home/dot_config/nvim/lua/plugins/telescope.nvim.lua
+++ home/dot_config/nvim/lua/plugins/telescope.nvim.lua
@@ -1,6 +1,7 @@
 -- https://github.com/nvim-telescope/telescope.nvim/blob/b4da76be54691e854d3e0e02c36b0245f945c2c7/README.md?plain=1#L121-L125
 return {
 	"nvim-telescope/telescope.nvim",
+	enabled = not vim.g.vscode,
 	dependencies = { "nvim-lua/plenary.nvim" },
 	-- https://github.com/nvim-telescope/telescope.nvim/blob/b4da76be54691e854d3e0e02c36b0245f945c2c7/README.md?plain=1#L160-L164
 	keys = {
