935e3d99d4a6ac74549178b6fb819be8c0a392cf
a <a@as-Virtual-Machine.local>
2025-08-31 22:26:39 -0700

    Introduce Tokyonight plugin config file

---
+++ home/dot_config/nvim/lua/plugins/tokyonight.nvim.lua
@@ -0,0 +1,7 @@
+-- https://github.com/folke/tokyonight.nvim/blob/0c68bc3876897613110a2f71340f2dc760c9c761/README.md?plain=1#L184-L189
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+}
