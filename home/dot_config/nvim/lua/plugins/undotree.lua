a6fddd83db4c825df68a36bab54275b4c41ffbb5
a <a@as-Virtual-Machine.local>
2025-08-31 23:04:15 -0700

    Disable Undotree in VS Code by conditionally enabling the plugin via vim.g.vscode

--- home/dot_config/nvim/lua/plugins/undotree.lua
+++ home/dot_config/nvim/lua/plugins/undotree.lua
@@ -1,5 +1,7 @@
 return {
 	"mbbill/undotree",
+	-- undotree doesn't work inside Visual Studio Code.
+	enabled = not vim.g.vscode,
 	keys = {
 		-- https://github.com/mbbill/undotree/blob/fe9a9d0645f0f5532360b5e5f5c550d7bb4f1869/README.md?plain=1#L80
 		{ "<leader><F5>", vim.cmd.UndotreeToggle },
