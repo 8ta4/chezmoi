37f06ebfc2364812cf107b7d6141916bd30d9917
a <a@as-Virtual-Machine.local>
2025-08-31 22:29:38 -0700

    Initialize Tokyonight in plugin setup to apply theme on startup

--- home/dot_config/nvim/lua/plugins/tokyonight.nvim.lua
+++ home/dot_config/nvim/lua/plugins/tokyonight.nvim.lua
@@ -1,7 +1,11 @@
 -- https://github.com/folke/tokyonight.nvim/blob/0c68bc3876897613110a2f71340f2dc760c9c761/README.md?plain=1#L184-L189
 return {
 	"folke/tokyonight.nvim",
+	config = function()
+		-- https://github.com/folke/tokyonight.nvim/blob/0c68bc3876897613110a2f71340f2dc760c9c761/README.md?plain=1#L195C1-L195C34
+		vim.cmd([[colorscheme tokyonight]])
+	end,
 	lazy = false,
-	priority = 1000,
 	opts = {},
+	priority = 1000,
 }
