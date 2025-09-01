c1099ddb6422e536b8cf64901871ec7009c61274
a <a@as-Virtual-Machine.local>
2025-08-31 22:33:51 -0700

    Disable Tokyonight when running in VS Code by gating with vim.g.vscode

--- home/dot_config/nvim/lua/plugins/tokyonight.nvim.lua
+++ home/dot_config/nvim/lua/plugins/tokyonight.nvim.lua
@@ -5,6 +5,8 @@ return {
 		-- https://github.com/folke/tokyonight.nvim/blob/0c68bc3876897613110a2f71340f2dc760c9c761/README.md?plain=1#L195C1-L195C34
 		vim.cmd([[colorscheme tokyonight]])
 	end,
+	-- tokyonight.nvim doesn't work inside Visual Studio Code.
+	enabled = not vim.g.vscode,
 	lazy = false,
 	opts = {},
 	priority = 1000,
