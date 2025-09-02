c7c7bd87e4e3caa4b93292c1e563857cff7d0466
a <a@as-Virtual-Machine.local>
2025-09-02 01:46:22 -0700

    Move Television file/text finder keybindings from Neovim config to VS Code keybindings.json

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -46,15 +46,6 @@ vim.cmd("lang en_US.UTF-8")
 -- https://github.com/LunarVim/LunarVim/issues/1857#issuecomment-1013641928
 vim.keymap.set("n", "<M-j>", "j", { noremap = true })
 vim.keymap.set("n", "<M-k>", "k", { noremap = true })
-if vim.g.vscode then
-	-- https://github.com/nvim-telescope/telescope.nvim/blob/b4da76be54691e854d3e0e02c36b0245f945c2c7/README.md?plain=1#L161-L162
-	vim.keymap.set("n", "<leader>ff", function()
-		require("vscode").call("television.ToggleFileFinder")
-	end, { desc = "Television find files" })
-	vim.keymap.set("n", "<leader>fg", function()
-		require("vscode").call("television.ToggleTextFinder")
-	end, { desc = "Television live grep" })
-end
 
 -- https://stackoverflow.com/questions/30691466/what-is-difference-between-vims-clipboard-unnamed-and-unnamedplus-settings
 vim.opt.clipboard = "unnamed,unnamedplus"
