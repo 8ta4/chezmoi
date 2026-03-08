25f02523d97206872b4d102d31cf402c3ffcfd51
a <a@as-Virtual-Machine.local>
2026-03-08 15:11:25 -0700

    Add conditional loading to disable windsurf.vim inside VS Code

--- home/dot_config/nvim/lua/plugins/windsurf.vim.lua
+++ home/dot_config/nvim/lua/plugins/windsurf.vim.lua
@@ -1,6 +1,11 @@
 -- https://github.com/Exafunction/windsurf.vim/blob/3c0a4f8a7be75113a6e19be13b7cc37210d6e26a/README.md?plain=1#L94-L104
 return {
 	"Exafunction/windsurf.vim",
+	-- windsurf.vim doesn't work inside Visual Studio Code.
+	-- https://github.com/vscode-neovim/vscode-neovim/wiki/Plugins/996cac1464fe73750faee0ea4ad51577410e66f0#:~:text=cond%20%3D%20(function()%20return%20not%20vim.g.vscode%20end)
+	cond = function()
+		return not vim.g.vscode
+	end,
 	config = function()
 		-- Change '<C-g>' here to any keycode you like.
 		vim.keymap.set("i", "<C-g>", function()
