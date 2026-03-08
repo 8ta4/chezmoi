6610f663b0253c0b69114296f8f97af1372d1860
a <a@as-Virtual-Machine.local>
2026-03-08 15:10:10 -0700

    Introduce windsurf.vim plugin configuration file

---
+++ home/dot_config/nvim/lua/plugins/windsurf.vim.lua
@@ -0,0 +1,19 @@
+-- https://github.com/Exafunction/windsurf.vim/blob/3c0a4f8a7be75113a6e19be13b7cc37210d6e26a/README.md?plain=1#L94-L104
+return {
+	"Exafunction/windsurf.vim",
+	config = function()
+		-- Change '<C-g>' here to any keycode you like.
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
+	end,
+}
