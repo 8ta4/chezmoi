c32ce5f8ddc7c00c7ccf9b4d2211cc850ec1898a
a <a@as-Virtual-Machine.local>
2026-03-10 00:24:54 -0700

    Add threshold-based exclusion to disable Flash search on large files

--- home/dot_config/nvim/lua/plugins/flash.nvim.lua
+++ home/dot_config/nvim/lua/plugins/flash.nvim.lua
@@ -1,16 +1,26 @@
+local threshold = 10000
 -- https://github.com/folke/flash.nvim/blob/54b70a91b1d116fffe974d69dae05a32c74b0ab0/README.md?plain=1#L64-L77
+-- https://github.com/vscode-neovim/vscode-neovim/discussions/2514#discussioncomment-15131078
 return {
 	"folke/flash.nvim",
-	-- https://github.com/vscode-neovim/vscode-neovim/discussions/2514#discussioncomment-15131078
-	-- Only enable flash.nvim when running inside Visual Studio Code.
-	-- It can be noticeably slow on large files, which I tend to open in a terminal neovim session rather than in Visual Studio Code.
-	-- https://github.com/vscode-neovim/vscode-neovim/wiki/Plugins/996cac1464fe73750faee0ea4ad51577410e66f0#:~:text=cond%20%3D%20(function()%20return%20vim.g.vscode%20end)
-	cond = function()
-		return vim.g.vscode
-	end,
 	event = "VeryLazy",
 	---@type Flash.Config
 	opts = {
+		-- https://github.com/folke/flash.nvim/blob/d83258c1e4d9da970db424a95464ee5595a0945b/README.md?plain=1#L120-L129
+		search = {
+			exclude = {
+				"notify",
+				"cmp_menu",
+				"noice",
+				"flash_prompt",
+				function(win)
+					-- exclude non-focusable windows
+					return not vim.api.nvim_win_get_config(win).focusable
+						-- flash.nvim can be noticeably slow on large files.
+						or vim.api.nvim_buf_line_count(vim.api.nvim_win_get_buf(win)) > threshold
+				end,
+			},
+		},
 		modes = {
 			search = {
 				enabled = true,
