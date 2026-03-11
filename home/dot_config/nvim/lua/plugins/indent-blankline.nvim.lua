b199bece0021f55fb175cb464c4b513feb220fdf
a <a@as-Virtual-Machine.local>
2026-03-10 21:46:01 -0700

    Add configuration file for indent‑blankline.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/indent-blankline.nvim.lua
@@ -0,0 +1,8 @@
+-- https://github.com/lukas-reineke/indent-blankline.nvim/blob/d28a3f70721c79e3c5f6693057ae929f3d9c0a03/README.md?plain=1#L17-L23
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {},
+}
