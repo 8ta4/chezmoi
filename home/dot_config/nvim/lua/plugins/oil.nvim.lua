09a44bf85e91bf44a7ca61e3a3c4cf0779b59c3b
a <a@as-Virtual-Machine.local>
2026-03-10 21:13:07 -0700

    Add oil.nvim plugin configuration with recommended settings.

---
+++ home/dot_config/nvim/lua/plugins/oil.nvim.lua
@@ -0,0 +1,12 @@
+-- https://github.com/stevearc/oil.nvim/blob/0fcc83805ad11cf714a949c98c605ed717e0b83e/README.md?plain=1#L36-L46
+return {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+}
