b27340cc6c589af656d4f53044b8dd6ebf2b40e9
a <a@as-Virtual-Machine.local>
2026-03-16 00:26:15 -0700

    Add VS Code compatibility checks to disable unsupported plugins

--- home/dot_config/nvim/lua/plugins/fennel.vim.lua
+++ home/dot_config/nvim/lua/plugins/fennel.vim.lua
@@ -1,1 +1,8 @@
-return { "bakpakin/fennel.vim" }
+return {
+	"bakpakin/fennel.vim",
+	-- fennel.vim doesn't work inside Visual Studio Code.
+	-- https://github.com/vscode-neovim/vscode-neovim/wiki/Plugins/996cac1464fe73750faee0ea4ad51577410e66f0#:~:text=cond%20%3D%20(function()%20return%20not%20vim.g.vscode%20end)
+	cond = function()
+		return not vim.g.vscode
+	end,
+}
--- home/dot_config/nvim/lua/plugins/nfnl.lua
+++ home/dot_config/nvim/lua/plugins/nfnl.lua
@@ -1,2 +1,10 @@
 -- https://github.com/Olical/nfnl/blob/f39a7ec6fe0ace2b2ea108ce04d954549e36c76d/README.md?plain=1#L174
-return { "Olical/nfnl", ft = "fennel" }
+return {
+	"Olical/nfnl",
+	-- nfnl doesn't work inside Visual Studio Code.
+	-- https://github.com/vscode-neovim/vscode-neovim/wiki/Plugins/996cac1464fe73750faee0ea4ad51577410e66f0#:~:text=cond%20%3D%20(function()%20return%20not%20vim.g.vscode%20end)
+	cond = function()
+		return not vim.g.vscode
+	end,
+	ft = "fennel",
+}
