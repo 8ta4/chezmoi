64acf02fc6c5187a5fd769868ee73e154bb0e9ed
a <a@as-Virtual-Machine.local>
2026-03-15 22:48:35 -0700

    Add reference link to lsp_signature.nvim documentation

--- home/dot_config/nvim/lua/plugins/lsp_signature.nvim.lua
+++ home/dot_config/nvim/lua/plugins/lsp_signature.nvim.lua
@@ -1,3 +1,4 @@
+-- https://github.com/ray-x/lsp_signature.nvim/blob/0efb088dce050d38a3608ee69f80f2a62cf9849c/README.md?plain=1#L62-L68
 return {
 	"ray-x/lsp_signature.nvim",
 	event = "InsertEnter",
