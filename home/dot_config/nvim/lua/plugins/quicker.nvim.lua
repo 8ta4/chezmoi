1fd779c455403559d64ffa3c2d50421bf6b79f9d
a <a@as-Virtual-Machine.local>
2026-05-05 18:35:28 -0700

    Introduce quicker.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/quicker.nvim.lua
@@ -0,0 +1,8 @@
+-- https://github.com/stevearc/quicker.nvim/blob/063cc44da1eef8681bbd653b29d3bc961780886a/README.md?plain=1#L51-L57
+return {
+	"stevearc/quicker.nvim",
+	ft = "qf",
+	---@module "quicker"
+	---@type quicker.SetupOptions
+	opts = {},
+}
